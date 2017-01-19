49bdf7211e99771859517e30afed7fa6ecd2b235
nguyenanh <nguyenanh@localhost.localdomain>
2017-01-19 16:40:30 +0700

    update index.php

--- usr/share/self-service-password/index.php
+++ usr/share/self-service-password/index.php
@@ -181,12 +181,12 @@ $mailer->LE            = $mail_newline;
 <div class="panel panel-success">
 <div class="panel-body">
 
-<?php if ( $result == "") { ?>
+<?php if ( $result) { ?>
 <div class="result alert alert-<?php echo get_criticity($result) ?>">
 <p><i class="fa <?php echo get_fa_class($result) ?>" aria-hidden="true"></i> <?php echo $messages[$result]; ?></p>
 </div>
 <?php } else {
-    include("pages/change.php");
+    include("pages/$action.php");
 } ?>
 
 </div>
